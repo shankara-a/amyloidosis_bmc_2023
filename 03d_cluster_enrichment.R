50a2e825b6c3d3e52f7a1c0d16f51ec39e4fbd63
Shankara Anand <shankara.k.anand@gmail.com>
2024-06-12 21:17:03 -0700

    revisions v1

--- 03d_cluster_enrichment.R
+++ 03d_cluster_enrichment.R
@@ -25,3 +25,12 @@ pdf("figures/full_stage2_renal1.pdf", w=10,h=8)
 options(repr.plot.width=10, repr.plot.height=8)
 grid.arrange(p1,p2,nrow=1)
 dev.off()
+
+# Reviewer Request
+f1 <- "data/clustering/full_na_dataset/subgroup_comparisons/contrasts_qvars_subgroup_low.tsv"
+contrasts.df <- read.table(f1, sep="\t", header=T)
+contrasts.df$cluster <- factor(contrasts.df$BU..BNP.based..cardiac.staging, levels=c("stage I","stage II","stage III","stage IIIb"), ordered=T)
+
+pdf("figures/subgroup_low_comparison.pdf", w=5,h=8)
+plotEnrichment(contrasts.df) + theme(axis.text.x = element_text(angle = 45, hjust = 1, size=10))
+dev.off()
